51f96eb9fc2fcfeb04fe984fb80d3c0a67b4dfc5
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-04 15:26:48 -0500

    Still Practicing

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,37 +1,38 @@
-import React from 'react'
+import React from 'react';
+import USVI from '../assets/USVI.jpg';
 
 
 const Home = () => {
     return (
         <React.Fragment>
-        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-interval="10000">
-                    <img src="..." class="d-block w-100" alt="..."/>
+        <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-interval="10000">
+                    <img src="..." className="d-block w-100" alt="..."/>
                 </div>
-                <div class="carousel-item" data-interval="2000">
-                    <img src="..." class="d-block w-100" alt="..."/>
+                <div className="carousel-item" data-interval="2000">
+                    <img src={USVI} className="d-block w-100" alt="..."/>
                 </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="..."/>
+                <div className="carousel-item">
+                    <img src="..." className="d-block w-100" alt="..."/>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+            <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+            <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
             </a>
             </div>
 
         <div className="card" style={{ width: "18rem" }}>
-            <img src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg" class="card-img-top" alt="..."/>
+            <img src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg" className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
         </React.Fragment>
